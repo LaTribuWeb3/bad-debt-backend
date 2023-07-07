be7f29183dbef8bb93cce4d202d384fa608757d3
Alfulinku <Alfulinku@gmail.com>
2023-07-07 15:10:22 +0200

    user GetRpcUrlForNetwork everywhere

--- src/runners/CompoundRunner.ts
+++ src/runners/CompoundRunner.ts
@@ -1,12 +1,13 @@
 import { CompoundParser } from '../parsers/compound/CompoundParser';
 import config from '../configs/CompoundRunnerConfig.json';
 import * as dotenv from 'dotenv';
+import { GetRpcUrlForNetwork } from '../utils/Utils';
 dotenv.config();
 
 async function RunCompoundParser() {
-  const rpcUrl = process.env.RPC_URL_ETH;
+  const rpcUrl = GetRpcUrlForNetwork(config.network);
   if (!rpcUrl) {
-    throw new Error('Could not find env variable "RPC_URL_ETH"');
+    throw new Error(`Could not find rpc url in env variable for network ${config.network}`);
   }
   const compoundParser = new CompoundParser(config, rpcUrl, 'ethereum_compound.json', 24, 1);
   await compoundParser.main();
--- src/runners/SonneRunner.ts
+++ src/runners/SonneRunner.ts
@@ -1,12 +1,13 @@
 import * as dotenv from 'dotenv';
 import { SonneParser } from '../parsers/compound/SonneParser';
 import config from '../configs/SonneRunnerConfig.json';
+import { GetRpcUrlForNetwork } from '../utils/Utils';
 dotenv.config();
 
 async function RunSonneParser() {
-  const rpcUrl = process.env.RPC_URL_OPTIMISM;
+  const rpcUrl = GetRpcUrlForNetwork(config.network);
   if (!rpcUrl) {
-    throw new Error('Could not find env variable "RPC_URL_OPTIMISM"');
+    throw new Error(`Could not find rpc url in env variable for network ${config.network}`);
   }
   const compoundParser = new SonneParser(config, rpcUrl, 'optimism_sonne.json', 24, 1);
   await compoundParser.main();
