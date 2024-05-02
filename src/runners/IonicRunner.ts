fedd94e9c02f5e44bfa3407cc30a7c0aca125949
Alfu <Alfulinku@gmail.com>
2024-05-02 11:08:58 +0200

    fix naming

--- src/runners/IonicRunner.ts
+++ src/runners/IonicRunner.ts
@@ -1,18 +1,18 @@
 import { CompoundParser } from '../parsers/compound/CompoundParser';
-import config from '../configs/ParseIonicConfig.json';
+import config from '../configs/IonicRunnerConfig.json';
 import * as dotenv from 'dotenv';
 import { GetRpcUrlForNetwork } from '../utils/Utils';
 dotenv.config();
 
-async function ParseIonicRunner() {
+async function IonicRunner() {
   const rpcUrl = GetRpcUrlForNetwork(config.network);
   if (!rpcUrl) {
     throw new Error(`Could not find rpc url in env variable for network ${config.network}`);
   }
 
-  const runnerName = 'ParseIonic-Runner';
+  const runnerName = 'IonerParser-Runner';
   const parser = new CompoundParser(config, runnerName, rpcUrl, 'mode_ionic.json', 24, 1);
   await parser.main();
 }
 
-ParseIonicRunner();
+IonicRunner();
