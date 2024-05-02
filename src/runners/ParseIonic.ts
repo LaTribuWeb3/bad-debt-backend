cd800c3e647cf289e7dece85bce8408719d60117
0x3lden <153280243+0x3lden@users.noreply.github.com>
2024-05-02 10:25:10 +0200

    Adds Parse Ionic Config

---
+++ src/runners/ParseIonic.ts
@@ -0,0 +1,18 @@
+import { CompoundParser } from '../parsers/compound/CompoundParser';
+import config from '../configs/CompoundRunnerConfig.json';
+import * as dotenv from 'dotenv';
+import { GetRpcUrlForNetwork } from '../utils/Utils';
+dotenv.config();
+
+async function ParseIonicRunner() {
+  const rpcUrl = GetRpcUrlForNetwork(config.network);
+  if (!rpcUrl) {
+    throw new Error(`Could not find rpc url in env variable for network ${config.network}`);
+  }
+
+  const runnerName = 'ParseIonic-Runner';
+  const parser = new CompoundParser(config, runnerName, rpcUrl, 'ethereum_ionic.json', 24, 1);
+  await parser.main();
+}
+
+ParseIonicRunner();
